dd56ce8a7b9f19c896729fd97e8f8a51bb77b2e1
EmiliasamaEMT <yangxinyu611@163.com>
2024-07-22 15:50:40 +0800

    114514

--- chapter2/full_name.py
+++ chapter2/full_name.py
@@ -1,5 +1,6 @@
-first_name="ada"
-last_name="lovelace"
+first_name="ada   "
+last_name="lovelace   "
 full_name=f"{first_name} {last_name}"
 print(full_name)
-print(f"Hello,{full_name.title()}!")
+message=f"Hello,\t{full_name.title()}\n!  "
+print(message.rstrip())
---
+++ chapter2/name_case.py
@@ -0,0 +1,10 @@
+first_name="\talan"
+last_name="\twalker\t"
+fullname=f"{first_name} {last_name}"
+number=114514
+print(f"Hello {fullname.title()},fuck u!")
+print(f"Hello {fullname.upper()},fuck u!")
+print(f"Hello {fullname.lower()},fuck u!\n{number}")
+print(f"{first_name.strip()}{last_name.lstrip()}")
+#zhessahdsdhkajsd
+
