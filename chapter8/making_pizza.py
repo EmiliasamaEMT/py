599019d4785d82946219e677b30a3cc2cbe47890
EmiliasamaEMT <yangxinyu611@163.com>
2024-07-26 15:31:56 +0800

    114514

---
+++ chapter8/making_pizza.py
@@ -0,0 +1,5 @@
+#import pizza
+#pizza.make_pizza(16,'musssjd','shdjkdhjd','sdsdjd')
+
+from pizza import make_pizza as ii
+ii(16,'musssjd','shdjkdhjd','sdsdjd')
---
+++ chapter8/pizza.py
@@ -0,0 +1,4 @@
+def make_pizza(size,*toppings):
+    print(f"make a {size} pizza ,use:")
+    for topping in toppings:
+        print(f" - {topping}")
